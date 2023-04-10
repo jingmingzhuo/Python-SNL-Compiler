b59ef9dec9cc874ccff4c36a009c4eee1aa7f26f
Jingming <jingmingzhuo@gmail.com>
2023-04-10 08:46:23 +0800

    feat : add config in main.py

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
 import sys, os
-import grammar.LL1 as grammar
+import grammar.LL1 as grammar1
+import grammar.recursiveDescent as grammar2
 import morer.morer as morer 
-import analyzer.semantic as analyser
+import analyzer.semantic as analyzer
+import argparse
 from util.logger import log, set_debug
+from util.config import get_cfg
 from formula.formula import generater
 
-def __get_token_and_tree(snlText: str):
-  token = ''
-  tree = ''
-  return token, tree;
 
 if __name__ == '__main__':
-  # run()
-  # grammar.run();
-  args = sys.argv
-  if len(args) < 2:
-    print('使用方法: python main.py <文件名>.snl [--debug] [-o <输出文件名>]')
-    os._exit(1)
 
-  if '--debug' in args:
-    set_debug()
 
-  output = 'dist/a.out'
-  if '-o' in args:
-    try:
-      output = 'dist/' + args[args.index('-o') + 1]
-    except Exception:
-      print('使用方法: python main.py <文件名>.snl [--debug] [-o <输出文件名>]')
-      os._exit(1)
+  parser = argparse.ArgumentParser(description='Training segmentation network')
+  parser.add_argument('--cfg',
+                        help='experiment config file address',
+                        default="configs/config.yaml",
+                        type=str)
+  args = parser.parse_args()
+  cfg = get_cfg()
+  cfg.merge_from_file(args.cfg)
+  cfg.freeze()
 
-  fileName = args[1]
+  fileName = cfg.MORER.SNL_TEXT
   snlText = ''
   with open(fileName, encoding='utf-8') as file:
     snlText = file.read()
 
   # 运行 词法分析器 根据SNLText 获取 TokenList
-  tokenList = morer.run(snlText)
+  tokenList = morer.run(cfg,snlText)
 
   log('\n'.join(map(lambda x: str(x), tokenList)))
 
-  # 运行 语法分析器 根据TokenList 获取 AST
-  tree = grammar.run(tokenList)
+  # 运行 LL1或递归下降语法分析器 根据TokenList 获取 GrammarTree
+  if cfg.GRAMMAR.GRAMMAR_METHOD=='LL1':
+    tree = grammar1.run(cfg, tokenList)
+  else:
+    tree = grammar2.run(cfg, tokenList)
   
-  # 通过 AST 和 Tokens 获取符号表信息、分析语义
-  _analyser = analyser.Analyzer(tokenList, tree)
-  _analyser.analyze()
+  # 通过 GrammarTree 和 Tokens 获取符号表信息、分析语义
+  _analyzer = analyzer.Analyzer(tokenList, tree)
+  _analyzer.analyze()
 
-  # TODO 中间代码优化
-  _generator = generater(tree)
-  formula_list = _generator.scan()
-  formula_list.show(output)
+  # 中间代码优化
+  output = cfg.FORMULA.OUT_ADDRESS
+  generator = generater(tree)
+  formula_list = generator.scan()
+  if cfg.FORMULA.CHOOSE=='sample':
+    formula_list.show2(output)
+  else :
+    formula_list.show(output)
 
   print('结果已生成在 dist 目录下')
